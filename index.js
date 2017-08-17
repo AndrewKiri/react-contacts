28eef95a2ad7e562c549897aec4bc825d7e15e16
andrewkiri <andrewkirichok@gmail.com>
2017-08-17 14:05:45 +0200

    removed index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-app.listen(3000, function () {
-    console.log('App listening on port 3000!')
-})

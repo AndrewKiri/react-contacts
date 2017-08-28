f56990533683b949e983b6ff55fa76b8ed62bdb7
andrewkiri <andrewkirichok@gmail.com>
2017-08-28 17:00:16 +0200

    cleared from unnecessary dependencies, clear react

--- public/js/react/App.js
+++ public/js/react/App.js
@@ -1,37 +1,9 @@
 import React, { Component } from 'react';
-import { request } from 'graphql-request';
-import List from './List';
 import Loader from './Loader';
 
 class App extends Component {
     constructor(props) {
         super(props);
-    }
-
-    getListData(cid = 1, offset = 0, limit = 25) {
-        let query = `query { 
-            tileset(cid: ${cid}, offset: ${offset}, limit: ${limit}, t: Article) {
-                id,
-                cid,
-                url,
-                title,
-                body(t: Plain){
-                data
-                },
-                img {
-                url,
-                h,
-                w,
-                author
-                },
-                tags,
-            }
-            }`;
-        request('https://mobileapi.wp.pl/v1/graphql?', query).then(data => console.log(data));
-    }
-
-    componentDidMount() {
-        this.getListData();
     }
 
     render() {

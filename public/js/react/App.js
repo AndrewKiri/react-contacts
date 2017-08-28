07bd0cd2fb24bc5ffacf241b21e88b178f439cc3
andrewkiri <andrewkirichok@gmail.com>
2017-08-28 18:16:52 +0200

    2 render methods

--- public/js/react/App.js
+++ public/js/react/App.js
@@ -4,14 +4,39 @@ import Loader from './Loader';
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: false
+        }
     }
 
-    render() {
-        return (
+    componentDidMount() {
+        this.setState({
+            loaded: true
+        })
+    }
+
+    renderLoader() {
+        return (            
             <div>
                 <Loader/>
             </div>
         );
+    }
+
+    renderApp() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+
+    render() {
+        if(this.state.loaded === true) {
+            return this.renderApp();
+        } else {
+            return this.renderLoader();
+        }
     }
 }
 
